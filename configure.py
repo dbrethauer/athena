edc311db8a037ada72c3b6a4dba0c36a19ba22ed
Daniel Brethauer <daniel_brethauer@berkeley.edu>
2026-04-15 10:27:24 -0700

    Adding personal directory to configure.py

--- configure.py
+++ configure.py
@@ -73,10 +73,15 @@ parser = argparse.ArgumentParser(description=athena_description, epilog=athena_e
 
 # --prob=[name] argument
 pgen_directory = 'src/pgen/'
+bonus_directory = '~/AthenaToKN'
 # set pgen_choices to list of .cpp files in src/pgen/
 pgen_choices = glob.glob(pgen_directory + '*.cpp')
+bonus_choices = glob.glob(bonus_directory+'*.cpp')
 # remove 'src/pgen/' prefix and '.cpp' extension from each filename
 pgen_choices = [choice[len(pgen_directory):-4] for choice in pgen_choices]
+bonus_choices = [choice[len(bonus_directory):-4] for choice in bonus_choices]
+
+pgen_choices += bonus_choices
 parser.add_argument('--prob',
                     default='shock_tube',
                     choices=pgen_choices,
